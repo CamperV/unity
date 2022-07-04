4ab2a5c394b35be3ce21a6cc4f7cf869daa369d3
Jay Danner <jaydanner@gmail.com>
2022-07-04 17:48:22 -0400

    Refactored AttackUC to properly use TargetableUC parent class

--- projects/TinyTactics/Assets/Scripts/Abilities/Commands/AttackUC.cs
+++ projects/TinyTactics/Assets/Scripts/Abilities/Commands/AttackUC.cs
@@ -6,19 +6,18 @@ using Extensions;
 using System.Linq;
 
 [CreateAssetMenu (menuName = "UnitCommands/AttackUC")]
-public class AttackUC : UnitCommand
+public class AttackUC : TargetableUC
 {
-    // we can actually keep some state here: there should never be two AttackUC's Activated at the same time
-    public static GridPosition _previousMouseOver; // for MoveSelection and AttackSelection (ContextualNoInteract)
-
     // waiting until an Engagement is done animating and resolving casualties
     public static bool _engagementResolveFlag = false;
-    
-    [SerializeField] private TileVisuals tileVisuals;
 
     public override void Activate(PlayerUnit thisUnit) {        
-        thisUnit.UpdateThreatRange(standing: true);
-        Utils.DelegateLateFrameTo(thisUnit, () => DisplayAttackRange(thisUnit));
+        thisUnit.UpdateThreatRange(
+            standing: true,
+            minRange: thisUnit.EquippedWeapon.MIN_RANGE,
+            maxRange: thisUnit.EquippedWeapon.MAX_RANGE
+        );
+        Utils.DelegateLateFrameTo(thisUnit, () => DisplayTargetRange(thisUnit));
     }
 
     public override void Deactivate(PlayerUnit thisUnit) {
@@ -31,60 +30,41 @@ public class AttackUC : UnitCommand
         UIManager.inst.DisableUnitDetail();
     }
 
-    public override ExitSignal ActiveInteractAt(PlayerUnit thisUnit, GridPosition interactAt, bool auxiliaryInteract) {
-        if (interactAt == thisUnit.gridPosition)
-            return ExitSignal.NoStateChange;
-
-        // if there's a ValidTarget on the mouseclick'd area
-        if (thisUnit.attackRange.ValidTarget(interactAt) && EnemyAt(thisUnit, interactAt) != null) {           
-            EnemyUnit enemy = EnemyAt(thisUnit, interactAt);
-
-            _engagementResolveFlag = true;
-            Engagement engagement = new Engagement(thisUnit, enemy);
-
-            Utils.DelegateCoroutineTo(thisUnit,
-                engagement.Resolve()
-            );
-
-            // wait until the engagement has ended
-            // once the engagement has processed, resolve the casualties
-            // once the casualties are resolved, EndTurnSelectedUnit()
-            Utils.DelegateCoroutineTo(thisUnit,
-                engagement.ExecuteAfterResolving(() => {
-                    _engagementResolveFlag = false;
-                })
-            );
-            
-            return ExitSignal.NextState;
-        }
-
-        // Incomplete -> "Do not change to InProgress state after returning"
-        return ExitSignal.NoStateChange;
+    protected override bool ValidTarget(PlayerUnit thisUnit, GridPosition interactAt) {
+        return thisUnit.attackRange.ValidTarget(interactAt) && EnemyAt(thisUnit, interactAt) != null;
     }
 
-    // this is where we show EngagementPreviews and the like
-    // this will fire on Update(), but should only fire when CommandActive is the state
-    public override void ActiveUpdate(PlayerUnit thisUnit) {
-        if (thisUnit.battleMap.CurrentMouseGridPosition != _previousMouseOver) {
-            _previousMouseOver = thisUnit.battleMap.CurrentMouseGridPosition;
+    protected override void Execute(PlayerUnit thisUnit, GridPosition interactAt) {
+        EnemyUnit enemy = EnemyAt(thisUnit, interactAt);
 
-            // reset these
-            DisplayAttackRange(thisUnit);
-            UIManager.inst.DisableEngagementPreview();
-            //
-            thisUnit.playerUnitController.Unlock();
-            
-            // when the mouse is over an enemy:
-            if (thisUnit.attackRange.ValidTarget(thisUnit.battleMap.CurrentMouseGridPosition) && EnemyAt(thisUnit, thisUnit.battleMap.CurrentMouseGridPosition) != null) {
-                thisUnit.playerUnitController.Lock();
-                //
-                EnemyUnit enemy = EnemyAt(thisUnit, thisUnit.battleMap.CurrentMouseGridPosition);
-                thisUnit.battleMap.Highlight(thisUnit.battleMap.CurrentMouseGridPosition, tileVisuals.altColor);
+        _engagementResolveFlag = true;
+        Engagement engagement = new Engagement(thisUnit, enemy);
 
-                // create and display EngagementPreviews here
-                UIManager.inst.EnableEngagementPreview( new Engagement(thisUnit, enemy), enemy.transform );
-            }
-        }
+        Utils.DelegateCoroutineTo(thisUnit,
+            engagement.Resolve()
+        );
+
+        // wait until the engagement has ended
+        // once the engagement has processed, resolve the casualties
+        // once the casualties are resolved, EndTurnSelectedUnit()
+        Utils.DelegateCoroutineTo(thisUnit,
+            engagement.ExecuteAfterResolving(() => {
+                _engagementResolveFlag = false;
+            })
+        );
+    }
+
+    protected override void ResetValidMouseOver(PlayerUnit thisUnit) {
+        DisplayTargetRange(thisUnit);
+        UIManager.inst.DisableEngagementPreview();
+    }
+
+    protected override void ValidMouseOver(PlayerUnit thisUnit, GridPosition hoverOver) {        
+        thisUnit.battleMap.Highlight(hoverOver, tileVisuals.altColor);
+
+        // create and display EngagementPreviews here
+        EnemyUnit enemy = EnemyAt(thisUnit, hoverOver);
+        UIManager.inst.EnableEngagementPreview( new Engagement(thisUnit, enemy), enemy.transform );
     }
 
     ////////////////////////////////////////////////////////////////////
@@ -129,10 +109,5 @@ public class AttackUC : UnitCommand
             validTargetForAnyWeapon |= enemyUC.activeUnits.Where(enemy => standing.ValidTarget(enemy.gridPosition)).Any();
         }
         return validTargetForAnyWeapon;
-    }
-
-    private void DisplayAttackRange(PlayerUnit thisUnit) {
-        thisUnit.attackRange.Display(thisUnit.battleMap, tileVisuals.color, tileVisuals.tile);
-        thisUnit.battleMap.Highlight(thisUnit.gridPosition, Palette.selectColorWhite);
     }
 }
