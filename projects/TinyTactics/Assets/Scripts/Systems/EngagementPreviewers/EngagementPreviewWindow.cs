7fcdca2a8f066193e343d46889e5554d718ea89d
Jay Danner <jaydanner@gmail.com>
2023-01-03 13:45:30 -0500

    minor WIP to switch computers

--- projects/TinyTactics/Assets/Scripts/Systems/EngagementPreviewers/EngagementPreviewWindow.cs
+++ projects/TinyTactics/Assets/Scripts/Systems/EngagementPreviewers/EngagementPreviewWindow.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Extensions;
 
 public class EngagementPreviewWindow : MonoBehaviour
 {
@@ -22,8 +23,11 @@ public class EngagementPreviewWindow : MonoBehaviour
 		PopulatePanels(potentialEngagement, enemyPreviewStats, isCounter: true);
 
 		// finally, deal with Unity wonkiness
-		foreach (ContentSizeFitter csf in GetComponentsInChildren<ContentSizeFitter>().Reverse()) {
-			Debug.Log($"Rebuilding {csf}");
+		// foreach (ContentSizeFitter csf in GetComponentsInChildren<ContentSizeFitter>().Reverse()) {
+		// 	LayoutRebuilder.ForceRebuildLayoutImmediate(csf.GetComponent<RectTransform>());
+		// }
+		foreach (ContentSizeFitter csf in GetComponentsInChildrenBFS<ContentSizeFitter>(gameObject)) {
+			Debug.Log($"csf {csf}");
 			LayoutRebuilder.ForceRebuildLayoutImmediate(csf.GetComponent<RectTransform>());
 		}
 	}
@@ -65,5 +69,24 @@ public class EngagementPreviewWindow : MonoBehaviour
 	private void CreateAndSet(string message, GameObject prefab, GameObject container) {
 		GameObject panel = Instantiate(prefab, container.transform);
 		panel.GetComponentInChildren<TextMeshProUGUI>().SetText(message);
+	}
+
+	private List<T> GetComponentsInChildrenBFS<T>(GameObject root) {
+		List<T> retval = new List<T>();
+		List<Transform> queue = new List<Transform>{root.transform};
+
+		while (queue.Count > 0) {
+			Transform current = queue.PopAt(0);
+
+			T comp = current.gameObject.GetComponent<T>();
+			if (comp != null) retval.Add(comp);
+
+			// all immediate children
+			foreach (Transform child in current) {
+				queue.Add(child);
+			}
+		}
+
+		return retval;
 	}
 }
