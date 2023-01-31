f6fe64da86f4c71990c1f9f12f6663002c2bada0
Jay Danner <jaydanner@gmail.com>
2023-01-31 10:35:17 -0500

    Changed buff sounds + colors

--- projects/TinyTactics/Assets/Scripts/Components/StatusAnimator.cs
+++ projects/TinyTactics/Assets/Scripts/Components/StatusAnimator.cs
@@ -20,6 +20,9 @@ public class StatusAnimator : MonoBehaviour
 
     [SerializeField] private AudioFXBundle genericBuffFXBundle;
     [SerializeField] private AudioFXBundle genericDebuffFXBundle;
+
+    [SerializeField] private Color buffColor;
+    [SerializeField] private Color debuffColor;
     
     void Awake() {
         statusSystem = GetComponent<StatusSystem>();
@@ -34,7 +37,7 @@ public class StatusAnimator : MonoBehaviour
 
     private void AnimateAddStatus(so_Status addedStatus) {
         if (addedStatus.hidden) return;
-        
+
         // queue the sound and animation for after it is done animating other things
         switch(addedStatus.statusCode) {
             case so_Status.StatusCode.Buff:
@@ -54,14 +57,14 @@ public class StatusAnimator : MonoBehaviour
     public void BuffAnimation(so_Status addedStatus) {
         audioSource.PlayOneShot(genericBuffFXBundle.RandomClip(), 1f);
         messageEmitter.Emit(MessageEmitter.MessageType.Buff, addedStatus.emitMessage);
-        StartCoroutine( spriteAnimator.FlashColorThenRevert(Palette.threatColorYellow) );
+        StartCoroutine( spriteAnimator.FlashColorThenRevert(buffColor) );
         StartCoroutine( spriteAnimator.SmoothCosX(32f, 0.015f, 0f, 1.0f) );
     }
 
     public void DebuffAnimation(so_Status addedStatus) {
         audioSource.PlayOneShot(genericDebuffFXBundle.RandomClip(), 1f);
         messageEmitter.Emit(MessageEmitter.MessageType.Debuff, addedStatus.emitMessage);
-        StartCoroutine( spriteAnimator.FlashColorThenRevert(Palette.threatColorIndigo) );
+        StartCoroutine( spriteAnimator.FlashColorThenRevert(debuffColor) );
         StartCoroutine( spriteAnimator.SmoothCosX(18f, 0.03f, 0f, 1.0f) );
     }
 }
